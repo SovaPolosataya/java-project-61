7c64ebfb5da26d8cc662a976d31cd4e8b97290e3
Irina Yukavskaya <strix1813@yandex.ru>
2023-11-16 23:58:45 +0300

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -4,57 +4,45 @@ import hexlet.code.Engine;
 import java.util.Random;
 
 public class Calc {
+    private static final String[] OPERATORS = {"+", "-", "*"};
+    public static int calculate(int randomNumber1, int randomNumber2, String operator) {
+        int answer = 0;
+        switch (operator) {
+            case "+":
+                answer = randomNumber1 + randomNumber2;
+                break;
+            case "-":
+                answer = randomNumber1 - randomNumber2;
+                break;
+            case "*":
+                answer = randomNumber1 * randomNumber2;
+                break;
+            default:
+                break;
+        }
+        return answer;
+    }
     public static void calcGame() {
         Random random = new Random();
 
         Engine.greeting();
-        System.out.println("What is the result of the expression?");
+        String task = "What is the result of the expression?";
 
-        int randomForTheSymbol;
-        String operator;
-        int answer;
-        String rightAnswer;
-        String question;
-        final int numberOfQuestions = 3;
+        final int maxRandomNumber = 100;
+        final int tasksNumber = 3;
         int randomNumber1;
         int randomNumber2;
-        final int maxRandomNumber = 100;
-        final int maxRandomForTheSymbol = 3;
-        int i = 1;
+        String randomOperator;
 
-        while (i <= numberOfQuestions) {
+        String[] questions = new String[tasksNumber];
+        String[] rightAnswers = new String[tasksNumber];
+        for (int y = 0; y < tasksNumber; y++) {
             randomNumber1 = random.nextInt(maxRandomNumber);
             randomNumber2 = random.nextInt(maxRandomNumber);
-            randomForTheSymbol = random.nextInt(maxRandomForTheSymbol);
-
-            switch (randomForTheSymbol) {
-                case 0:
-                    operator = " + ";
-                    answer = randomNumber1 + randomNumber2;
-                    break;
-                case 1:
-                    operator = " - ";
-                    answer = randomNumber1 - randomNumber2;
-                    break;
-                default:
-                    operator = " * ";
-                    answer = randomNumber1 * randomNumber2;
-            }
-            rightAnswer = Integer.toString(answer);
-            question = randomNumber1 + operator + randomNumber2;
-            System.out.println("Question: " + question);
-
-            System.out.print("Your answer: ");
-            String userAnswer = Engine.getScanner().next();
-
-            if (userAnswer.equals(rightAnswer)) {
-                System.out.println("Correct!");
-                i = i + 1;
-            } else {
-                Engine.evaluationOfAnswers(userAnswer, rightAnswer);
-                break;
-            }
+            randomOperator = OPERATORS[random.nextInt(OPERATORS.length - 1)];
+            questions[y] = randomNumber1 + " " + randomOperator + " " + randomNumber2;
+            rightAnswers[y] = Integer.toString(calculate(randomNumber1, randomNumber2, randomOperator));
         }
-        Engine.congratulations();
+        Engine.gameShell(task, questions, rightAnswers);
     }
 }
