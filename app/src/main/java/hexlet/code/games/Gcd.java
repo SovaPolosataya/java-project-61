df55232e786a2be946fb31675b5634ac7a692127
Irina Yukavskaya <strix1813@yandex.ru>
2023-11-17 00:08:01 +0300

    update Gcd.java

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -28,34 +28,22 @@ public class Gcd {
         Random random = new Random();
 
         Engine.greeting();
-        System.out.println("Find the greatest common divisor of given numbers.");
+        String task = "Find the greatest common divisor of given numbers.";
 
-        String rightAnswer;
-        String question;
-        final int numberOfQuestions = 3;
+        final int maxRandomNumber = 100;
+        final int tasksNumber = 3;
         int randomNumber1;
         int randomNumber2;
-        final int maxRandomNumber = 100;
-        int i = 1;
 
-        while (i <= numberOfQuestions) {
+        String[] questions = new String[tasksNumber];
+        String[] rightAnswers = new String[tasksNumber];
+
+        for (int y = 0; y < tasksNumber; y++) {
             randomNumber1 = random.nextInt(maxRandomNumber);
             randomNumber2 = random.nextInt(maxRandomNumber);
-            question = randomNumber1 + " " + randomNumber2;
-            rightAnswer = Integer.toString(nodGsd(randomNumber1, randomNumber2));
-            System.out.println("Question: " + question);
-
-            System.out.print("Your answer: ");
-            String userAnswer = Engine.getScanner().next();
-
-            if (userAnswer.equals(rightAnswer)) {
-                System.out.println("Correct!");
-                i = i + 1;
-            } else {
-                Engine.evaluationOfAnswers(userAnswer, rightAnswer);
-                break;
-            }
+            questions[y] =  randomNumber1 + " " + randomNumber2;
+            rightAnswers[y] = Integer.toString(nodGsd(randomNumber1, randomNumber2));
         }
-        Engine.congratulations();
+        Engine.gameShell(task, questions, rightAnswers);
     }
 }
