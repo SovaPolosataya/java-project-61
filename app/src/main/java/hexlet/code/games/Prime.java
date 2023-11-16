1fde4c9cc5eb3f4adf8bd7428a0fe71dc8ef71b0
Irina Yukavskaya <strix1813@yandex.ru>
2023-11-17 00:40:35 +0300

    fixed Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -7,7 +7,7 @@ public class Prime {
         if (randomNumber < 2) {
             answer = "no";
         }
-        for (int j = 2; j < randomNumber / 2; j++) {
+        for (int j = 2; j <= randomNumber / 2; j++) {
             if (randomNumber % j == 0) {
                 answer = "no";
                 break;
@@ -18,13 +18,16 @@ public class Prime {
     }
     public static void primeGame() {
         Random random = new Random();
-        Engine.greeting();
+
         String task = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-        final int maxRandomNumber = 100;
-        final int tasksNumber = 3;
         int randomNumber;
-        String[] questions = new String[tasksNumber];
+        final int tasksNumber = 3;
+        final int maxRandomNumber = 100;
+
+        Engine.greeting();
         String[] rightAnswers = new String[tasksNumber];
+        String[] questions = new String[tasksNumber];
+
         for (int y = 0; y < tasksNumber; y++) {
             randomNumber = random.nextInt(maxRandomNumber);
             questions[y] = Integer.toString(randomNumber);
