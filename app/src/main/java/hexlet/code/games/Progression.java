134f228bc808761b90e174a767754ac7bc2bc582
Irina Yukavskaya <strix1813@yandex.ru>
2023-11-17 00:09:19 +0300

    update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -7,45 +7,38 @@ public class Progression {
         Random random = new Random();
 
         Engine.greeting();
-        System.out.println("What number is missing in the progression?");
+        String task = "What number is missing in the progression?";
 
-        String rightAnswer = "";
-        final int numberOfQuestions = 3;
-        int randomNumber;
-        int randomProgressionStep;
-        int unknownNumber;
         final int progressionLength = 10;
         final int maxRandomNumber = 30;
         final int maxRandomProgressionStep = 9;
         final int maxUnknownNumber = 10;
-        int i = 1;
+        final int tasksNumber = 3;
+        int randomProgressionStep;
+        int unknownNumber;
+        int randomNumber;
+        String rightAnswer = "";
 
-        while (i <= numberOfQuestions) {
+        String[] questions = new String[tasksNumber];
+        String[] rightAnswers = new String[tasksNumber];
+        for (int y = 0; y < tasksNumber; y++) {
             randomNumber = random.nextInt(1, maxRandomNumber);
             randomProgressionStep = random.nextInt(2, maxRandomProgressionStep);
             unknownNumber = random.nextInt(1, maxUnknownNumber);
-            System.out.print("Question: ");
+            StringBuilder sequence = new StringBuilder();
             for (int j = 1; j <= progressionLength; j++) {
                 if (j == unknownNumber) {
                     rightAnswer = Integer.toString(randomNumber);
-                    System.out.print(".." + " ");
+                    sequence.append(".. ");
                     randomNumber += randomProgressionStep;
                 }
-                System.out.print(randomNumber + " ");
+                sequence.append(randomNumber);
+                sequence.append(" ");
                 randomNumber += randomProgressionStep;
             }
-            System.out.println("");
-            System.out.print("Your answer: ");
-            String userAnswer = Engine.getScanner().next();
-
-            if (userAnswer.equals(rightAnswer)) {
-                System.out.println("Correct!");
-                i = i + 1;
-            } else {
-                Engine.evaluationOfAnswers(userAnswer, rightAnswer);
-                break;
-            }
+            questions[y] = sequence.toString();
+            rightAnswers[y] = rightAnswer;
         }
-        Engine.congratulations();
+        Engine.gameShell(task, questions, rightAnswers);
     }
 }
