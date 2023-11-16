3c7e17b02e1e08a84bbb828a05821c7fcffc7336
Irina Yukavskaya <strix1813@yandex.ru>
2023-11-17 00:00:17 +0300

    update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -4,40 +4,27 @@ import java.util.Random;
 import hexlet.code.Engine;
 
 public class Even {
+    public static String isEven(int randomNumber) {
+        return randomNumber % 2 == 0 ? "yes" : "no";
+    }
     public static void evenGame() {
         Random random = new Random();
 
         Engine.greeting();
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        String task = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
-        String rightAnswer;
-        String yes = "yes";
-        String no = "no";
-        final int numberOfQuestions = 3;
-        int randomNumber;
         final int maxRandomNumber = 1000;
-        int i = 1;
+        final int tasksNumber = 3;
+        int randomNumber;
 
-        while (i <= numberOfQuestions) {
+        String[] questions = new String[tasksNumber];
+        String[] rightAnswers = new String[tasksNumber];
+
+        for (int y = 0; y < tasksNumber; y++) {
             randomNumber = random.nextInt(maxRandomNumber);
-            if (randomNumber % 2 == 0) {
-                rightAnswer = yes;
-            } else {
-                rightAnswer = no;
-            }
-            System.out.println("Question: " + randomNumber);
-
-            System.out.print("Your answer: ");
-            String userAnswer = Engine.getScanner().next();
-
-            if (userAnswer.equals(rightAnswer)) {
-                System.out.println("Correct!");
-                i = i + 1;
-            } else {
-                Engine.evaluationOfAnswers(userAnswer, rightAnswer);
-                break;
-            }
+            questions[y] = Integer.toString(randomNumber);
+            rightAnswers[y] = isEven(randomNumber);
         }
-        Engine.congratulations();
+        Engine.gameShell(task, questions, rightAnswers);
     }
 }
