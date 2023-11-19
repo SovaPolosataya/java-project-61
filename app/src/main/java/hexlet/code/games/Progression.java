ce36c28efa3cf9b7c2f60698b357b300914f7dc4
Irina Yukavskaya <strix1813@yandex.ru>
2023-11-19 18:07:20 +0300

    update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,40 +1,51 @@
 package hexlet.code.games;
-import java.util.Random;
+
+import hexlet.code.Utils;
 import hexlet.code.Engine;
+import static hexlet.code.Engine.NUMBER_OF_QUESTION;
+import java.util.Arrays;
+
 public class Progression {
+    public static int[] isProgression(int randomNumber, int randomProgressionStep, int progressionLength) {
+        int[] sequences = new int[progressionLength];
+
+        for (int j = 0; j < progressionLength; j++) {
+            sequences[j] = randomNumber;
+            randomNumber += randomProgressionStep;
+        }
+        return sequences;
+    }
     public static void progressionGame() {
-        Random random = new Random();
-        Engine.greeting();
         String task = "What number is missing in the progression?";
         final int progressionLength = 10;
         final int maxRandomNumber = 30;
         final int maxRandomProgressionStep = 9;
-        final int maxUnknownNumber = 10;
-        final int tasksNumber = 3;
+        final int maxUnknownNumber = 9;
         int randomProgressionStep;
         int unknownNumber;
         int randomNumber;
-        String rightAnswer = "";
-        String[] questions = new String[tasksNumber];
-        String[] rightAnswers = new String[tasksNumber];
-        for (int y = 0; y < tasksNumber; y++) {
-            randomNumber = random.nextInt(1, maxRandomNumber);
-            randomProgressionStep = random.nextInt(2, maxRandomProgressionStep);
-            unknownNumber = random.nextInt(1, maxUnknownNumber);
-            StringBuilder sequence = new StringBuilder();
-            for (int j = 1; j <= progressionLength; j++) {
-                if (j == unknownNumber) {
-                    rightAnswer = Integer.toString(randomNumber);
-                    sequence.append(".. ");
-                    randomNumber += randomProgressionStep;
-                }
-                sequence.append(randomNumber);
-                sequence.append(" ");
-                randomNumber += randomProgressionStep;
-            }
-            questions[y] = sequence.toString();
-            rightAnswers[y] = rightAnswer;
+        int rightAnswer;
+        int flagOfMissingNumber = -1;
+        String question;
+
+        String[] questions = new String[NUMBER_OF_QUESTION];
+        String[] rightAnswers = new String[NUMBER_OF_QUESTION];
+
+        for (int y = 0; y < NUMBER_OF_QUESTION; y++) {
+            randomNumber = Utils.isRandom(1, maxRandomNumber);
+            randomProgressionStep = Utils.isRandom(2, maxRandomProgressionStep);
+            unknownNumber = Utils.isRandom(maxUnknownNumber);
+
+            int[] sequences = isProgression(randomNumber, randomProgressionStep, progressionLength);
+            rightAnswer = sequences[unknownNumber];
+            sequences[unknownNumber] = flagOfMissingNumber;
+
+            question  = Arrays.toString(sequences).replaceAll("[\\[\\],]", "")
+                    .replace("-1", "..");
+
+            questions[y] = question;
+            rightAnswers[y] = Integer.toString(rightAnswer);
         }
-        Engine.gameShell(task, questions, rightAnswers);
+        Engine.runGame(task, questions, rightAnswers);
     }
 }
