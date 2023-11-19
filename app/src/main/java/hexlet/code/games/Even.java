295e8506f15cde7ad40ae9ae2b71337dc9e4c4cb
Irina Yukavskaya <strix1813@yandex.ru>
2023-11-19 18:05:55 +0300

    update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,24 +1,28 @@
 package hexlet.code.games;
-import java.util.Random;
+
+import hexlet.code.Utils;
 import hexlet.code.Engine;
+import static hexlet.code.Engine.NUMBER_OF_QUESTION;
+
+
 public class Even {
-    public static String isEven(int randomNumber) {
-        return randomNumber % 2 == 0 ? "yes" : "no";
+    public static boolean isEven(int randomNumber) {
+        return randomNumber % 2 == 0;
     }
     public static void evenGame() {
-        Random random = new Random();
-        Engine.greeting();
         String task = "Answer 'yes' if the number is even, otherwise answer 'no'.";
         final int maxRandomNumber = 1000;
-        final int tasksNumber = 3;
         int randomNumber;
-        String[] questions = new String[tasksNumber];
-        String[] rightAnswers = new String[tasksNumber];
-        for (int y = 0; y < tasksNumber; y++) {
-            randomNumber = random.nextInt(maxRandomNumber);
+
+        String[] questions = new String[NUMBER_OF_QUESTION];
+        String[] rightAnswers = new String[NUMBER_OF_QUESTION];
+
+        for (int y = 0; y < NUMBER_OF_QUESTION; y++) {
+            randomNumber = Utils.isRandom(maxRandomNumber);
+
             questions[y] = Integer.toString(randomNumber);
-            rightAnswers[y] = isEven(randomNumber);
+            rightAnswers[y] = isEven(randomNumber) ? "yes" : "no";
         }
-        Engine.gameShell(task, questions, rightAnswers);
+        Engine.runGame(task, questions, rightAnswers);
     }
 }
