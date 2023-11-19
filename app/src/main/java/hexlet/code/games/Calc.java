4a8e54fe16b1421043da30670b9463aaa02cd79e
Irina Yukavskaya <strix1813@yandex.ru>
2023-11-19 18:05:27 +0300

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,10 +1,15 @@
 package hexlet.code.games;
+
 import hexlet.code.Engine;
-import java.util.Random;
+import static hexlet.code.Engine.NUMBER_OF_QUESTION;
+import hexlet.code.Utils;
+
 public class Calc {
     private static final String[] OPERATORS = {"+", "-", "*"};
+
     public static int calculate(int randomNumber1, int randomNumber2, String operator) {
         int answer = 0;
+
         switch (operator) {
             case "+":
                 answer = randomNumber1 + randomNumber2;
@@ -20,24 +25,26 @@ public class Calc {
         }
         return answer;
     }
+
     public static void calcGame() {
-        Random random = new Random();
-        Engine.greeting();
         String task = "What is the result of the expression?";
         final int maxRandomNumber = 100;
-        final int tasksNumber = 3;
         int randomNumber1;
         int randomNumber2;
+        int operatorsLength = OPERATORS.length - 1;
         String randomOperator;
-        String[] questions = new String[tasksNumber];
-        String[] rightAnswers = new String[tasksNumber];
-        for (int y = 0; y < tasksNumber; y++) {
-            randomNumber1 = random.nextInt(maxRandomNumber);
-            randomNumber2 = random.nextInt(maxRandomNumber);
-            randomOperator = OPERATORS[random.nextInt(OPERATORS.length - 1)];
+
+        String[] questions = new String[NUMBER_OF_QUESTION];
+        String[] rightAnswers = new String[NUMBER_OF_QUESTION];
+
+        for (int y = 0; y < NUMBER_OF_QUESTION; y++) {
+            randomNumber1 = Utils.isRandom(maxRandomNumber);
+            randomNumber2 = Utils.isRandom(maxRandomNumber);
+            randomOperator = OPERATORS[Utils.isRandom(operatorsLength)];
+
             questions[y] = randomNumber1 + " " + randomOperator + " " + randomNumber2;
             rightAnswers[y] = Integer.toString(calculate(randomNumber1, randomNumber2, randomOperator));
         }
-        Engine.gameShell(task, questions, rightAnswers);
+        Engine.runGame(task, questions, rightAnswers);
     }
 }
